859d56aad6e8b7be134afc7ed64f231df24c0bf1
mondos <mondos01@gmail.com>
2018-08-05 14:22:03 +0100

    Split out js and css code

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+function encrypt(){
+    let passhash=CryptoJS.SHA256(document.getElementById("account").value);
+    let out=document.getElementById("hashedNumber");
+    let cpy=document.getElementById("output");
+    cpy.style.display='inline-block';
+    out.innerText="";
+    out.innerText= passhash;
+};
+function CopyToClipboard(){
+let inp =document.createElement('input');
+document.body.appendChild(inp);
+inp.value =document.getElementById('output').textContent.replace(/\s/g,'');
+inp.select();
+document.execCommand('copy',false);
+inp.remove();
+}
+var e = document.getElementById('account');
+e.oninput = encrypt;
+
+tippy('[title]');
+tippy('#tips', { target: '.btn-class-copy' });
